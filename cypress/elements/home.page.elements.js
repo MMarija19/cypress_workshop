7fcc5353621b0f0204418c97ae902675fec9b43c
Marija Milosavljevic <marijamilosavljevic@Marijas-MacBook-Air.local>
2023-02-06 00:05:05 +0100

    bad commit

--- cypress/elements/home.page.elements.js
+++
@@ -1,7 +0,0 @@
-export default class HomePageElements {
-  categoryCards = ".card-body";
-  cardMenu = ".header-wrapper";
-  header = 'a[href="https://demoqa.com"]';
-  menuOptionsList = ".element-group";
-  subMenuOptionsList = ".menu-list li";
-}
